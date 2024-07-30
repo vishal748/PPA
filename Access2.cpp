149a22f4339084181ca1603cb440bc617b255d62
vishal748 <112422729+vishal748@users.noreply.github.com>
2024-07-30 23:26:46 +0530

    Add files via upload

---
+++ Access2.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    public:
+        int A;
+    private:
+        int B;
+    protected:
+        int C;
+    public:
+        Base()
+        {   A = 10; B = 20; C = 30; }
+        void Fun()
+        {
+            cout<<A<<"\n";  // A
+            cout<<B<<"\n";  // A
+            cout<<C<<"\n";  // A
+        }
+};
+class Derived 
+{
+    public:
+        void Gun()
+        {
+            cout<<A<<"\n";  // A
+            cout<<B<<"\n";  // NA
+            cout<<C<<"\n";  // A         
+        }
+};
+
+int main()
+{
+    Base bobj;
+
+    cout<<bobj.A<<"\n"; // A 
+    cout<<bobj.B<<"\n"; // NA
+    cout<<bobj.C<<"\n"; // NA
+
+
+    Derived dobj;
+    cout<<bobj.A<<"\n"; // A 
+    cout<<bobj.B<<"\n"; // NA
+    cout<<bobj.C<<"\n";//NA
+
+    return 0;
+}
